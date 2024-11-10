296b84fdee1fb37d0db4bc8a01203ab6a188fea0
mitrank <mitrankshah@yahoo.com>
2024-11-10 14:41:13 +0530

    completed bottom tabs

--- app/index.jsx
+++ app/index.jsx
@@ -4,9 +4,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-200">
-      <Text className="text-3xl">Aora</Text>
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to Profile
+      <Text className="text-3xl font-pblack">Aora</Text>
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
       </Link>
     </View>
   );
