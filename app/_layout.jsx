d526601f9aa2fd55b5ccefcbf2b776746c525709
mitrank <mitrankshah@yahoo.com>
2024-11-10 21:07:58 +0530

    1. Included the nativewind ts config to work with types 2. Removed unnecessary import statements

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,3 @@
-import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect } from "react";
 import { Slot, SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Redirect, router } from "expo-router";
+import { router } from "expo-router";
 import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
