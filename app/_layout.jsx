6fa39dabeed6a784ccc216b36b831b5933e9af51
mitrank <mitrankshah@yahoo.com>
2024-12-02 02:55:01 +0530

    removed sentry and re-configured the app env properties

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,8 @@
 import React, { useEffect } from "react";
-import { Slot, SplashScreen, Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import "../global.css";
 import { GlobalProvider } from "../context/GlobalProvider";
-import * as Sentry from "@sentry/react-native";
-
-Sentry.init({
-  dsn: "https://ac3fcd43490c5dd306f6d3e8a8bd9f80@o4508371000950784.ingest.us.sentry.io/4508371002785792",
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // enableSpotlight: __DEV__,
-});
 
 SplashScreen.preventAutoHideAsync();
 
@@ -46,4 +38,4 @@ const RootLayout = () => {
   );
 };
 
-export default Sentry.wrap(RootLayout);
+export default RootLayout;
